002712448ceb59ef878945ca7fbbb16455ca40fa
Willians H <wh.140291@gmail.com>
2025-09-14 16:29:54 -0500

    listo el home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import styles from './Home.module.css';
-import Typewriter from './Typewriter';
 
 const Home = () => {
   return (
     <section id="home" className={styles.Home}>
+      <div className={styles.shape1}></div>
+      <div className={styles.shape2}></div>
+      <div className={styles.shape3}></div>
+      <div className={styles.shape4}></div>
+      <div className={styles.shape5}></div>
       <div className={styles.content}>
-        <h1>
-          <Typewriter text="Soluciones de Software a la Medida para tu Negocio" />
-        </h1>
+        <img src="/Logo/Logo-01.jpg" alt="BiAse IT Solutions Logo" className={styles.logo} />
+        <h1>Soluciones de Software a la Medida para tu Negocio</h1>
         <p>En BiAse IT Solutions ofrecemos desarrollo personalizado, consultoría tecnológica y reclutamiento de talento IT para impulsar tu empresa.</p>
       </div>
     </section>
